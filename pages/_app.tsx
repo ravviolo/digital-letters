5047dd72ff3f0bda72a82fe273accef2e7d596bc
Jolanta Ślipko <j.slipko21@gmail.com>
2022-07-22 08:14:14 +0200

    Add chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { AuthProvider } from '../context/auth';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
-export default MyApp
+export default MyApp;
