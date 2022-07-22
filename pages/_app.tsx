4e0bce5561edac45cbcf985c18de230ac917f63c
Jolanta Ślipko <j.slipko21@gmail.com>
2022-07-22 08:34:35 +0200

    Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../context/auth';
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
